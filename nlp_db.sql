d1d2862aa93705bdb4c4635c9660d779f94d303b
NLP_ICAN <jessgrunblatt@Macintosh.local>
2018-02-08 12:24:02 -0900

    db dump updates

--- nlp_db.sql
+++ nlp_db.sql
@@ -3,24 +3,16 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Nov 04, 2017 at 12:14 AM
+-- Generation Time: Feb 08, 2018 at 10:23 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `nlp_db`
 --
-CREATE DATABASE IF NOT EXISTS `nlp_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-USE `nlp_db`;
 
 -- --------------------------------------------------------
 
@@ -33,7 +25,7 @@ CREATE TABLE `projects` (
   `name` varchar(28) COLLATE utf8_bin NOT NULL,
   `user_id` int(100) NOT NULL,
   `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `projects`
@@ -71,7 +63,10 @@ INSERT INTO `projects` (`project_id`, `name`, `user_id`, `created_on`) VALUES
 (79, 'TestProject', 62, '2017-10-08 21:48:23'),
 (80, 'dummyTest', 62, '2017-10-23 22:27:16'),
 (81, 'MobyDick', 16, '2017-10-26 19:39:25'),
-(82, 'newest', 69, '2017-10-29 20:23:09');
+(82, 'newest', 69, '2017-10-29 20:23:09'),
+(83, 'project_one', 72, '2018-01-24 01:35:40'),
+(84, 'project_one', 73, '2018-01-24 01:36:52'),
+(85, 'project_one', 74, '2018-02-05 01:31:46');
 
 -- --------------------------------------------------------
 
@@ -84,7 +79,7 @@ CREATE TABLE `tokens` (
   `token` varchar(255) COLLATE utf8_bin NOT NULL,
   `user_id` int(100) NOT NULL,
   `created` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `tokens`
@@ -155,7 +150,11 @@ INSERT INTO `tokens` (`id`, `token`, `user_id`, `created`) VALUES
 (71, '3547e47619e6dc185f7db5ebe430f9', 67, '2017-08-23'),
 (72, '0e09ce1d3ca2315160d7a64dd2fd27', 68, '2017-09-24'),
 (73, 'b342336e1dad086cb107c6b6bd5081', 69, '2017-09-24'),
-(74, '646efc4a03bc1f02b580b35e00dae5', 70, '2017-10-20');
+(74, '646efc4a03bc1f02b580b35e00dae5', 70, '2017-10-20'),
+(75, '19de7090358b2bee7c0951a9fd2eb6', 71, '2017-11-21'),
+(76, 'a15602bb1443b30d34c9835ed2cf79', 72, '2018-01-24'),
+(77, 'a65d00a74cce6b5a6f4bd6406fd072', 73, '2018-01-24'),
+(78, '9bfdc9a95cc99a92b70151c299de58', 74, '2018-02-05');
 
 -- --------------------------------------------------------
 
@@ -184,37 +183,41 @@ CREATE TABLE `users` (
   `layout` int(11) NOT NULL DEFAULT '0',
   `mod_resolution` double NOT NULL DEFAULT '0.4',
   `project_id` int(16) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `firstName`, `lastName`, `role`, `last_login`, `status`, `file_dir`, `use_freq`, `freq_lower_bound`, `freq_upper_bound`, `date_range`, `skew_x`, `skew_y`, `skew_z`, `shape`, `layout`, `mod_resolution`, `project_id`) VALUES
-(4, 'ching@gmail.com', '$2y$10$khPMD7VjuxpH2vGyPwiLeONet3yrABElw8VgvhFNxTRr2JOvic026', 'Boyd', 'Ching', 'subscriber', '2016-06-29 06:07:18 AM', 'approved', '/Users/jessgrunblatt/users-uaa/ching@gmail.com', NULL, NULL, NULL, 0, 0, 0, 0, '', 0, 0, NULL),
-(8, 'byching@alaska.edu', '$2y$10$OIJlLw6m57OooQ3FVZjIr.Ldgb1euP6onfgO2amkRPNFYylr5fzNu', 'Boyd', 'Ching', 'subscriber', '2016-07-21 10:14:41 PM', 'approved', '/Users/jessgrunblatt/users-uaa/byching@alaska.edu', NULL, NULL, NULL, 0, 0, 0, 0, '', 0, 0, NULL),
-(9, 'loyster1@alaska.edu', '$2y$10$N6yjQm0Bv4oAYXDg04c33.qNVFRuOFBP802OAqjbW6cwPVQWRa1o6', 'Levi', 'Oyster', 'subscriber', '2017-07-20 11:00:30 PM', 'approved', '/Users/jessgrunblatt/users-uaa/loyster1@alaska.edu', 0, 1, 40, 12, 0, 0, 0, '', 0, 0, NULL),
-(11, 'motion411@gmail.com', '$2y$10$paOhccdordpY7bdKVchlYOKfbjUU7rYa4th3EZldVOp8pWUfD3T6O', 'Levi', 'Oyster', 'subscriber', '2016-10-15 04:04:30 AM', 'approved', '/Users/jessgrunblatt/users-uaa/motion411@gmail.com', NULL, NULL, NULL, 0, 0, 0, 0, '', 0, 0, NULL),
-(15, 'peter@dadJokes.com', '$2y$10$xa01oz7Ljq8.hFKlh.OtO.9oDEJ.z7bJoO7FPEM5kZNGnsyv/X2h2', 'Peter', 'Peter', 'subscriber', '2017-08-23 04:56:51 AM', 'approved', '/Users/jessgrunblatt/users-uaa/peter@dadJokes.com/test', 1, 35, 5, 90, 2, 2, 10000, 'hybrid', 0, 0.4, 76),
-(16, 'mcenek@alaska.edu', '$2y$10$oaTxT9Uey9UYL/yYxjunLujkDEEBNcfN5z4RpcXsdPXsfr1FcM2Im', 'Martin', 'Cenek', 'subscriber', '2017-11-03 11:40:59 PM', 'approved', '/Users/jessgrunblatt/users-uaa/mcenek@alaska.edu/AustraliaBroadcast', 1, 30, 10, 1, 1, 1, 500, 'hybrid', 0, 0.75, 73),
-(33, 'Mc@Donald.com', '$2y$10$vN46vxjgQ5Brm4icLSY.OeQ2Lfr1d16k.zqQ5/QINHDGzVaXDDOl.', 'Ronald', 'McDonald', 'subscriber', '2017-05-22 05:33:16 AM', 'approved', '/Users/jessgrunblatt/users-uaa/Mc@Donald.com/klkl', 0, 80, 2, 91, 1, 1, 80, 'hybrid', 0, 0.4, 50),
-(54, 'bob@bob.com', '$2y$10$p4aaRFVfPv9HwMfqAQZqOevVUGCRBcATfzRSvUbETqK4F.eGHqVge', 'bob', 'bob', 'subscriber', '2017-05-14 11:43:48 AM', 'approved', '/Users/jessgrunblatt/users-uaa/bob@bob.com/new', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 44),
-(55, 'newguya@safd.com', '$2y$10$DwHZx661nBvhRR7IyvwkhOduMUopcIcQnihqPqOgv3ec4iPnCUJEi', 'asdfag', 'gsd', 'subscriber', '2017-05-15 11:14:01 PM', 'approved', '/Users/jessgrunblatt/users-uaa/newguya@safd.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 46),
-(56, 'pak.eric@gmail.com1', '$2y$10$2FQTycRvkmlaT6Yxu/bNI.iMDztIXQcXTQ82ZtSNGTbLTkkaBLinK', 'eric', 'pak', 'subscriber', '2017-05-15 11:15:05 PM', 'approved', '/Users/jessgrunblatt/users-uaa/pak.eric@gmail.com1/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 47),
-(57, 'rowan.bulkow@gmail.com', '$2y$10$c0hPUuQHqifotnFFzBJiE..esMY17FRBAOqOL9ZOHBDFW6w0CzytG', 'rowan', 'bulkow', 'subscriber', '2017-08-01 06:22:45 AM', 'approved', '/Users/jessgrunblatt/users-uaa/rowan.bulkow@gmail.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 63),
-(58, 'james@lown.com', '$2y$10$5QsC6eooxcqA5/oxIpdQ6.BYNg.pn8BAWmXDxHO..9ZYR58OYFlfG', 'james', 'lown', 'subscriber', '2017-08-05 07:57:03 AM', 'approved', '/Users/jessgrunblatt/users-uaa/james@lown.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 64),
-(59, 'james@pak.com', '$2y$10$6mfogNMfbOvvbeO11iXb7eg7gp249IdsfxcWeajBziNemw8C1.aCe', 'James', 'Pak', 'subscriber', '2017-08-05 08:03:23 AM', 'approved', '/Users/jessgrunblatt/users-uaa/james@pak.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 65),
-(60, 'snap@snap.com', '$2y$10$Eo1P3vhn5AvgKn9gvFRcdevKDo7sE0y/pLCKbpcZdAfOKbggDPSAS', 'snap', 'snap', 'subscriber', '2017-08-07 10:49:48 AM', 'approved', '/Users/jessgrunblatt/users-uaa/snap@snap.com/poop', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 67),
-(61, 'jgrunbla@alaska.edu', '', 'Jess', 'Grunblatt', 'subscriber', '', 'pending', NULL, 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
-(62, 'rbulkow2@alaska.edu', '$2y$10$uZ8xjk0fMjKfaDwKZPZEM.VUCFs2BgksIvf/NOwagYWLgJUFhbjXC', 'Rowan', 'Bulkow', 'subscriber', '2017-11-03 11:56:25 PM', 'approved', '/Users/jessgrunblatt/users-uaa/rbulkow2@alaska.edu/dummyTest', 1, 100, 1, 91, 1, 1, 1, 'hybrid', 0, 0.4, 80),
-(63, 'drcenek@gmail.com', '$2y$10$401QxlwsXMNTemH1cYKw2uOp3ttLbGHWFsO9HXq.inETV3DYF5Li6', 'Martin', 'MartinC', 'subscriber', '2017-08-20 02:28:35 AM', 'approved', '/Users/jessgrunblatt/users-uaa/drcenek@gmail.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 70),
-(64, 'jess@gina.alaska.edu', '$2y$10$cKmCojCIjPxmQmROt91HWu1liOEmddleqA/MNNo5bDfhDIeXxbtaa', 'Jess', 'Grunblatt', 'subscriber', '2017-08-21 11:40:42 PM', 'approved', '/Users/jessgrunblatt/users-uaa/jess@gina.alaska.edu/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 71),
-(65, 'eric@test.com', '', 'eric', 'test', 'subscriber', '', 'pending', NULL, 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
-(66, 'this@test.com', '', 'this', 'test', 'subscriber', '', 'pending', NULL, 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
-(67, 'first@last.com', '', 'first', 'last', 'subscriber', '', 'pending', NULL, 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
-(68, 'drodriguez9@alaska.edu', '$2y$10$v7fp1zZ1rg3puuE.hiXqpuK/80ptxvGotUkaKnK6aL9KmX.ww10p6', 'David', 'Rodriguez', 'subscriber', '2017-09-24 10:54:19 PM', 'approved', '/Users/jessgrunblatt/users-uaa/drodriguez9@alaska.edu/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 77),
-(69, 'josephf95@gmail.com', '$2y$10$fDCTw7Qtd2AHS8QcwXIlaehyNPv2tHQOlDpS1OaglHVG1R9op2Fem', 'Joe', 'Fiskeaux', 'subscriber', '2017-10-29 09:15:07 PM', 'approved', '/Users/jessgrunblatt/users-uaa/josephf95@gmail.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 78),
-(70, 'howyouvegrown@email.net', '', 'Mackenzie', 'Zaleska', 'subscriber', '', 'pending', NULL, 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL);
+(4, 'ching@gmail.com', '$2y$10$khPMD7VjuxpH2vGyPwiLeONet3yrABElw8VgvhFNxTRr2JOvic026', 'Boyd', 'Ching', 'subscriber', '2016-06-29 06:07:18 AM', 'approved', '/Users/jessgrunblatt/users-uaa/ching@gmail.com', 0, 0.1, 0.9, 0, 0, 0, 0, '', 0, 0, NULL),
+(8, 'byching@alaska.edu', '$2y$10$OIJlLw6m57OooQ3FVZjIr.Ldgb1euP6onfgO2amkRPNFYylr5fzNu', 'Boyd', 'Ching', 'subscriber', '2016-07-21 10:14:41 PM', 'approved', '/Users/jessgrunblatt/users-uaa/byching@alaska.edu', 0, 0.1, 0.9, 0, 0, 0, 0, '', 0, 0, NULL),
+(9, 'loyster1@alaska.edu', '$2y$10$N6yjQm0Bv4oAYXDg04c33.qNVFRuOFBP802OAqjbW6cwPVQWRa1o6', 'Levi', 'Oyster', 'subscriber', '2017-07-20 11:00:30 PM', 'approved', '/Users/jessgrunblatt/users-uaa/loyster1@alaska.edu', 0, 0.1, 0.9, 12, 0, 0, 0, '', 0, 0, NULL),
+(11, 'motion411@gmail.com', '$2y$10$paOhccdordpY7bdKVchlYOKfbjUU7rYa4th3EZldVOp8pWUfD3T6O', 'Levi', 'Oyster', 'subscriber', '2016-10-15 04:04:30 AM', 'approved', '/Users/jessgrunblatt/users-uaa/motion411@gmail.com', 0, 0.1, 0.9, 0, 0, 0, 0, '', 0, 0, NULL),
+(15, 'peter@dadJokes.com', '$2y$10$xa01oz7Ljq8.hFKlh.OtO.9oDEJ.z7bJoO7FPEM5kZNGnsyv/X2h2', 'Peter', 'Peter', 'subscriber', '2017-08-23 04:56:51 AM', 'approved', '/Users/jessgrunblatt/users-uaa/peter@dadJokes.com/test', 0, 0.1, 0.9, 90, 2, 2, 10000, 'hybrid', 0, 0.4, 76),
+(16, 'mcenek@alaska.edu', '$2y$10$oaTxT9Uey9UYL/yYxjunLujkDEEBNcfN5z4RpcXsdPXsfr1FcM2Im', 'Martin', 'Cenek', 'subscriber', '2018-01-26 03:06:01 AM', 'approved', '/Users/jessgrunblatt/users-uaa/mcenek@alaska.edu/dummyTest', 1, 0.05, 0.95, 1, 5, 5, 200, 'hybrid', 0, 0.75, 74),
+(33, 'Mc@Donald.com', '$2y$10$vN46vxjgQ5Brm4icLSY.OeQ2Lfr1d16k.zqQ5/QINHDGzVaXDDOl.', 'Ronald', 'McDonald', 'subscriber', '2017-05-22 05:33:16 AM', 'approved', '/Users/jessgrunblatt/users-uaa/Mc@Donald.com/klkl', 0, 0.1, 0.9, 91, 1, 1, 80, 'hybrid', 0, 0.4, 50),
+(54, 'bob@bob.com', '$2y$10$p4aaRFVfPv9HwMfqAQZqOevVUGCRBcATfzRSvUbETqK4F.eGHqVge', 'bob', 'bob', 'subscriber', '2017-05-14 11:43:48 AM', 'approved', '/Users/jessgrunblatt/users-uaa/bob@bob.com/new', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 44),
+(55, 'newguya@safd.com', '$2y$10$DwHZx661nBvhRR7IyvwkhOduMUopcIcQnihqPqOgv3ec4iPnCUJEi', 'asdfag', 'gsd', 'subscriber', '2017-05-15 11:14:01 PM', 'approved', '/Users/jessgrunblatt/users-uaa/newguya@safd.com/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 46),
+(56, 'pak.eric@gmail.com1', '$2y$10$2FQTycRvkmlaT6Yxu/bNI.iMDztIXQcXTQ82ZtSNGTbLTkkaBLinK', 'eric', 'pak', 'subscriber', '2017-05-15 11:15:05 PM', 'approved', '/Users/jessgrunblatt/users-uaa/pak.eric@gmail.com1/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 47),
+(57, 'rowan.bulkow@gmail.com', '$2y$10$c0hPUuQHqifotnFFzBJiE..esMY17FRBAOqOL9ZOHBDFW6w0CzytG', 'rowan', 'bulkow', 'subscriber', '2017-08-01 06:22:45 AM', 'approved', '/Users/jessgrunblatt/users-uaa/rowan.bulkow@gmail.com/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 63),
+(58, 'james@lown.com', '$2y$10$5QsC6eooxcqA5/oxIpdQ6.BYNg.pn8BAWmXDxHO..9ZYR58OYFlfG', 'james', 'lown', 'subscriber', '2017-08-05 07:57:03 AM', 'approved', '/Users/jessgrunblatt/users-uaa/james@lown.com/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 64),
+(59, 'james@pak.com', '$2y$10$6mfogNMfbOvvbeO11iXb7eg7gp249IdsfxcWeajBziNemw8C1.aCe', 'James', 'Pak', 'subscriber', '2017-08-05 08:03:23 AM', 'approved', '/Users/jessgrunblatt/users-uaa/james@pak.com/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 65),
+(60, 'snap@snap.com', '$2y$10$Eo1P3vhn5AvgKn9gvFRcdevKDo7sE0y/pLCKbpcZdAfOKbggDPSAS', 'snap', 'snap', 'subscriber', '2017-08-07 10:49:48 AM', 'approved', '/Users/jessgrunblatt/users-uaa/snap@snap.com/poop', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 67),
+(61, 'jgrunbla@alaska.edu', '', 'Jess', 'Grunblatt', 'subscriber', '', 'pending', NULL, 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
+(62, 'rbulkow2@alaska.edu', '$2y$10$uZ8xjk0fMjKfaDwKZPZEM.VUCFs2BgksIvf/NOwagYWLgJUFhbjXC', 'Rowan', 'Bulkow', 'subscriber', '2018-02-08 10:21:49 PM', 'approved', '/Users/jessgrunblatt/users-uaa/rbulkow2@alaska.edu/dummyTest', 1, 0.1, 0.8, 91, 1, 1, 1, 'hybrid', 0, 0.4, 80),
+(63, 'drcenek@gmail.com', '$2y$10$401QxlwsXMNTemH1cYKw2uOp3ttLbGHWFsO9HXq.inETV3DYF5Li6', 'Martin', 'MartinC', 'subscriber', '2017-08-20 02:28:35 AM', 'approved', '/Users/jessgrunblatt/users-uaa/drcenek@gmail.com/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 70),
+(64, 'jess@gina.alaska.edu', '$2y$10$cKmCojCIjPxmQmROt91HWu1liOEmddleqA/MNNo5bDfhDIeXxbtaa', 'Jess', 'Grunblatt', 'subscriber', '2018-01-25 12:15:12 AM', 'approved', '/Users/jessgrunblatt/users-uaa/jess@gina.alaska.edu/project_one', 1, 0.1, 0.5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 71),
+(65, 'eric@test.com', '', 'eric', 'test', 'subscriber', '', 'pending', NULL, 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
+(66, 'this@test.com', '', 'this', 'test', 'subscriber', '', 'pending', NULL, 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
+(67, 'first@last.com', '', 'first', 'last', 'subscriber', '', 'pending', NULL, 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
+(68, 'drodriguez9@alaska.edu', '$2y$10$v7fp1zZ1rg3puuE.hiXqpuK/80ptxvGotUkaKnK6aL9KmX.ww10p6', 'David', 'Rodriguez', 'subscriber', '2017-09-24 10:54:19 PM', 'approved', '/Users/jessgrunblatt/users-uaa/drodriguez9@alaska.edu/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 77),
+(69, 'josephf95@gmail.com', '$2y$10$fDCTw7Qtd2AHS8QcwXIlaehyNPv2tHQOlDpS1OaglHVG1R9op2Fem', 'Joe', 'Fiskeaux', 'subscriber', '2017-12-23 05:51:23 AM', 'approved', '/Users/jessgrunblatt/users-uaa/josephf95@gmail.com/project_one', 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, 78),
+(70, 'howyouvegrown@email.net', '', 'Mackenzie', 'Zaleska', 'subscriber', '', 'pending', NULL, 0, 0.1, 0.9, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
+(71, '', '', '', '', 'subscriber', '', 'pending', NULL, 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, NULL),
+(72, 'pereilly@alaska.edu', '$2y$10$qfJdI9ue.M4Ghs8dB.RqJeYzNB6f.isFm5a4fm90uDRTfbZ8269rK', 'Patricia', 'Reilly', 'subscriber', '2018-01-24 02:37:08 AM', 'approved', '/Users/jessgrunblatt/users-uaa/pereilly@alaska.edu/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 83),
+(73, 'ddonnally@alaska.edu', '$2y$10$VKWBi.K/Tg2VNvQRfXwrD.vFQLLqGIJPEtksG2uURWOW4RcComaUq', 'Dave', 'Donnally', 'subscriber', '2018-02-05 07:28:00 AM', 'approved', '/Users/jessgrunblatt/users-uaa/ddonnally@alaska.edu/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 84),
+(74, 'Triciaereilly@gmail.com', '$2y$10$ecXtlNheMOx9Xe0ZqLyZNuRYkpP9cuiOdMhqqsvIZpfNlh.aRqhJ2', 'Patricia', 'Reilly', 'subscriber', '2018-02-05 02:36:10 AM', 'approved', '/Users/jessgrunblatt/users-uaa/Triciaereilly@gmail.com/project_one', 0, 25, 5, 91, 1, 1, 1, 'hybrid', 0, 0.4, 85);
 
 --
 -- Indexes for dumped tables
@@ -249,17 +252,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(16) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=83;
+  MODIFY `project_id` int(16) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=86;
 --
 -- AUTO_INCREMENT for table `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=75;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=79;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=71;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=75;
 --
 -- Constraints for dumped tables
 --
@@ -275,7 +278,3 @@ ALTER TABLE `projects`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `project_id_fk` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`) ON DELETE SET NULL;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

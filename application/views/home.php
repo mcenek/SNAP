2bbc2ddf03a5f405e3f4b6e08a6137b6c9c2191a
rowan-bulkow <rowan.bulkow@gmail.com>
2018-08-17 10:58:50 -0800

    Code formatting, css

--- application/views/home.php
+++ application/views/home.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <link rel="stylesheet" href="<?php echo asset_url(); ?>css/menuStyle.css" type="text/css" />
+    <?php include 'resources.php'; ?>
 
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <link rel='stylesheet' type='text/css' href='//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="<?php echo asset_url(); ?>css/menuStyle.css" type="text/css" />
 
     <title>Home Page</title>
 </head>
 <body>
     <?php include 'navi.php';?>
-    <div class="container">
 
-    <h1>Home</h1>
-    <h2>Welcome <?php echo $firstName; ?></h2><br/>
+    <div class="container">
+        <h1>Home</h1>
+        <h2>Welcome <?php echo $firstName; ?></h2>
     </div>
 </body>
 </html>
